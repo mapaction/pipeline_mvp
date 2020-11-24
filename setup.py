27b40febee4a7bf3fc5a7da74a2e66826051065c
Jan van der Vegt <jan.vandervegt@cubonacci.com>
2020-11-24 15:29:36 +0100

    Initial setup for new structure

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='pipeline_mvp',
-    packages=setuptools.find_packages(),
-    setup_requires=[],
-    tests_require=[],
-    install_requires=[],
-)
