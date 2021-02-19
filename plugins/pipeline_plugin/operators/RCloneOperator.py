1709d9b2d4ab31325892b6a0cae1042baba4a556
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2021-02-19 21:39:09 +0000

    move location of temp file + debug code

--- plugins/pipeline_plugin/operators/RCloneOperator.py
+++ plugins/pipeline_plugin/operators/RCloneOperator.py
@@ -13,35 +13,56 @@ logger = logging.getLogger(__name__)
 
 def sync_from_gcp_to_gdrive(gcp_path: str, gdrive_folder_id: str):
 
-    _, service_account_auth_path = tempfile.mkstemp(suffix='.json')
+    _, service_account_auth_path = tempfile.mkstemp(dir='/usr/local/airflow/plugins', suffix='.json')
     logger.info(f'creating temporary service auth file = {service_account_auth_path}')
     logger.info(f'temporary service auth file exists (expect true) = {os.path.exists(service_account_auth_path)}')
     logger.info(f'temporary service auth file size (expect zero) = {os.path.getsize(service_account_auth_path)}')
 
     try:
-        logger.info(f'Attempting to update temporary service auth file from GoogleCloudStorageClient')
+        if config.is_inside_gcp():
+            logger.info(f'Attempting to update temporary service auth file from GoogleCloudStorageClient')
+            gcsc = GoogleCloudStorageClient()
+            gcsc.download_file_from_gcs(
+                bucket_name=config.get_rclone_service_account_auth_bucket(),
+                source_blob=config.get_rclone_service_account_auth_file(),
+                destination_filename=service_account_auth_path
+            )
 
-        gcsc = GoogleCloudStorageClient()
-        gcsc.download_file_from_gcs(
-            bucket_name=config.get_rclone_service_account_auth_bucket(),
-            source_blob=config.get_rclone_service_account_auth_file(),
-            destination_filename=service_account_auth_path
-        )
+            logger.info(f'temporary service auth file exists (expect true) = {os.path.exists(service_account_auth_path)}')
+            logger.info(f'temporary service auth file size (expect non-zero) = {os.path.getsize(service_account_auth_path)}')
 
-        logger.info(f'temporary service auth file exists (expect true) = {os.path.exists(service_account_auth_path)}')
-        logger.info(f'temporary service auth file size (expect non-zero) = {os.path.getsize(service_account_auth_path)}')
+            rclone_cmd = [f'rclone',
+                f'sync',
+                f':"google cloud storage":{gcp_path}',
+                f'--gcs-service-account-file={service_account_auth_path}',
+                f':drive:',
+                f'--drive-scope=drive',
+                f'--drive-service-account-file={service_account_auth_path}"',
+                f'--drive-team-drive={gdrive_folder_id}',
+                f'--drive-auth-owner-only']
 
-        rclone_cmd = [f'rclone',
-            f'sync',
-            f':"google cloud storage":{gcp_path}',
-            f'--gcs-service-account-file={service_account_auth_path}',
-            f':drive:',
-            f'--drive-scope=drive',
-            f'--drive-service-account-file={service_account_auth_path}"',
-            f'--drive-team-drive={gdrive_folder_id}',
-            f'--drive-auth-owner-only']
+            subprocess.check_call(rclone_cmd)
+        else:
+            logger.info(f'Attempting to update temporary service auth file from GoogleCloudStorageClient')
 
-        subprocess.check_call(rclone_cmd)
+            test_cmd = [f'ls',
+                f'-la',
+                f'{service_account_auth_path}'
+            ]
+
+            logger.info(f'temporary service auth file exists (expect true) = {os.path.exists(service_account_auth_path)}')
+            logger.info(f'temporary service auth file size (expect non-zero) = {os.path.getsize(service_account_auth_path)}')
+
+            subprocess.check_call(test_cmd)          
+            logger.info(f'compeleted with real file')
+
+            test_cmd = [f'ls',
+                f'-la',
+                f'{service_account_auth_path}doesnotexist'
+            ]
+            subprocess.check_call(test_cmd)          
+            logger.info(f'compeleted with non existant file')
+
     finally:
         os.remove(service_account_auth_path)
 
