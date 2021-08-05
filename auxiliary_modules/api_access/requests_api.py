1c3ac194ef83087a55977917cc4536559f4165ee
ego <zzwerruga@mail.ru>
2021-08-05 11:44:30 +0300

    added unit tests for requests api

--- auxiliary_modules/api_access/requests_api.py
+++ auxiliary_modules/api_access/requests_api.py
@@ -1,4 +1,3 @@
-import json
 import logging
 
 import requests
@@ -13,48 +12,53 @@ def download_url(url, save_path, chunk_size=1024, parameters=None):
             fd.write(chunk)
     logger.info(f'Downloaded "{url}" to "{save_path}"')
     logger.info(f"Status code is {r.status_code}")
-    # return r.status_code
 
 
-def get_json(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        print("Did not get 200 response from query.")
-        return False
-    jr = json.loads(r.text)
-    return jr
+"""
+Both get_json and SessionWithHeaderRedirection were inherited from datasources-etl.
+They were used to work with srtm data. We do not use srtm data now. But we might in the future.
+"""
+# TODO: either use these functions with srtm or delete them.
 
-
-###############################################################################
-class SessionWithHeaderRedirection(requests.Session):
-    """
-    overriding requests.Session.rebuild_auth to maintain
-    headers when redirected. Pinched off wiki (Aug 2020)
-    https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
-    """
-
-    AUTH_HOST = "urs.earthdata.nasa.gov"
-
-    def __init__(self, username, password):
-        super().__init__()
-        self.auth = (username, password)
-
-    # Overrides from the library to keep headers when redirected to or from
-    # the NASA auth host.
-
-    def rebuild_auth(self, prepared_request, response):
-
-        headers = prepared_request.headers
-        url = prepared_request.url
-
-        if "Authorization" in headers:
-            original_parsed = requests.utils.urlparse(response.request.url)
-            redirect_parsed = requests.utils.urlparse(url)
-
-            if (
-                original_parsed.hostname != redirect_parsed.hostname
-                and redirect_parsed.hostname != self.AUTH_HOST
-                and original_parsed.hostname != self.AUTH_HOST
-            ):
-                del headers["Authorization"]
-        return None
+# def get_json(url):
+#     r = requests.get(url)
+#     if r.status_code != 200:
+#         print("Did not get 200 response from query.")
+#         return False
+#     jr = json.loads(r.text)
+#     return jr
+#
+#
+# ###############################################################################
+# class SessionWithHeaderRedirection(requests.Session):
+#     """
+#     overriding requests.Session.rebuild_auth to maintain
+#     headers when redirected. Pinched off wiki (Aug 2020)
+#     https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
+#     """
+#
+#     AUTH_HOST = "urs.earthdata.nasa.gov"
+#
+#     def __init__(self, username, password):
+#         super().__init__()
+#         self.auth = (username, password)
+#
+#     # Overrides from the library to keep headers when redirected to or from
+#     # the NASA auth host.
+#
+#     def rebuild_auth(self, prepared_request, response):
+#
+#         headers = prepared_request.headers
+#         url = prepared_request.url
+#
+#         if "Authorization" in headers:
+#             original_parsed = requests.utils.urlparse(response.request.url)
+#             redirect_parsed = requests.utils.urlparse(url)
+#
+#             if (
+#                 original_parsed.hostname != redirect_parsed.hostname
+#                 and redirect_parsed.hostname != self.AUTH_HOST
+#                 and original_parsed.hostname != self.AUTH_HOST
+#             ):
+#                 del headers["Authorization"]
+#         return None
